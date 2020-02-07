a0025f9fcf7c151202214912a73906411169b119
joao.blehm <joao.blehm@GX2-137.gx2.local>
2020-02-07 18:58:57 -0200

    criação da classe fatura

--- OperadoraTabajara/src/pagamento/Fatura.java
+++ OperadoraTabajara/src/pagamento/Fatura.java
@@ -1,5 +1,16 @@
 package pagamento;
 
+import br.com.tabajaradigital.negocio.Operadora;
+import br.com.tabajaradigital.negocio.Plano;
+import calculo.CalculoInternet;
+import calculo.CalculoMinuto;
+
 public class Fatura {
+	
+	private Plano Plano;
+	private Operadora Operadora;
+	private CalculoInternet CalculoInternet;
+	private CalculoMinuto CalculoMinuto;
+	
 
 }
